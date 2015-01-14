e1948dbbee4a4fc2a222fc4c138c8d7dbb62490d
Anton Mihailov <antonmihailov@abv.bg>
2015-01-14 09:42:30 +0200

    Revert "Adding some pages"
    This reverts commit 74b148e2b82cfe75334456fc728af591eff8af24.

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,26 +4,6 @@ class MainController extends Phalcon\Mvc\Controller
 {
 	public function indexAction()
 	{
-	}
-	
-	public function missionAction()
-	{
-	}
-	
-	public function organizationAction()
-	{
-		$this->view->setLayout('organization');
-	}
-	
-	public function constructionAction()
-	{
-	}
-	
-	public function commandingSpaceShipAction()
-	{
-	}
-	
-	public function aboutAction()
-	{
+		echo 'A spaceship is coming...';
 	}
 }
